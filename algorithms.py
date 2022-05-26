4d9ae9c76a77f8390f8b9c4e8af82037810d593e
codeinchicago <vancewcole@gmail.com>
2022-05-26 08:22:08 -0500

    Finished problem 3.

--- algorithms.py
+++ algorithms.py
@@ -37,10 +37,31 @@ for word in text:
     elif word.lower() in my_dict:
         my_dict[word.lower()] += 1
 
-print(my_dict)
+# print(my_dict)
 
 # z = sorted(my_dict.items())
 # print(type(z))
 
 #Sorting dictionary by words, do not want to change dictionary itself.
 # print(my_dict.sort(my_dict.keys()))
+
+## Exercise #3
+
+"""
+Write a function implementing a Linear Search Algorithm. A linear search is a method for finding an element within a list. It sequentially checks each element of the list until a match is found or the whole list has been searched. If you do not find a match, return -1
+"""
+
+nums_list = [10,23,45,70,11,15]
+target = 70
+
+def linear_search(nums_list, target):
+    for num in nums_list:
+        if num == target:
+            return True
+    return -1
+
+print(linear_search(nums_list, 70))
+print(linear_search(nums_list, 75))
+
+# If number is not present return -1
+            
